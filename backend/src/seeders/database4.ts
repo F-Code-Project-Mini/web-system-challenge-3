0a78a5feae94a5ad34a7e7eb53b9c648fed4542c
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-20 14:27:34 +0700

    feat: add data interview

---
+++ backend/src/seeders/database4.ts
@@ -0,0 +1,31 @@
+import prisma from "~/configs/prisma";
+import { resumeList } from "./data-raw/resume";
+import Resume from "~/schemas/resume.schema";
+
+const seed = async () => {
+    const count = await prisma.resume.count();
+    if (count > 0) {
+        console.log("Database has been seeded. Exiting...");
+        return;
+    }
+
+    for (let i = 0; i < resumeList.length; i++) {
+        const resumeData = resumeList[i];
+        const candidate = await prisma.candidate.findFirst({
+            where: {
+                studentCode: resumeData.student_code,
+            },
+        });
+
+        if (candidate) {
+            await prisma.resume.create({
+                data: {
+                    candidateId: candidate.id,
+                    filePath: resumeData.final_submit,
+                },
+            });
+        }
+    }
+};
+
+seed();
