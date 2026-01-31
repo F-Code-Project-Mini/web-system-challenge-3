df3623a88b76262e9ed1e2a7682d457e49f187a3
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-31 21:29:04 +0700

    feat: change status realtime

--- frontend/src/pages/Admin/components/ApproveMember.tsx
+++ frontend/src/pages/Admin/components/ApproveMember.tsx
@@ -9,16 +9,21 @@ import {
 } from "~/components/ui/select";
 
 import { Socket } from "socket.io-client";
+import type { StatusC3 } from "~/types/user.types";
 type ApproveMemberProps = {
     value: string;
     candidateId: string;
     socket?: Socket;
+    handleStatusChange?: (candidateId: string, status: StatusC3) => void;
 };
-const ApproveMember = ({ value, candidateId, socket }: ApproveMemberProps) => {
+const ApproveMember = ({ value, candidateId, socket, handleStatusChange }: ApproveMemberProps) => {
     return (
         <Select
             defaultValue={value}
-            onValueChange={(newValue) => socket?.emit("APPROVE_CANDIDATE", { candidateId, status: newValue })}
+            onValueChange={(newValue) => {
+                socket?.emit("APPROVE_CANDIDATE", { candidateId, status: newValue });
+                handleStatusChange?.(candidateId, newValue as StatusC3);
+            }}
         >
             <SelectTrigger className="w-full max-w-48 bg-white">
                 <SelectValue placeholder="Chọn trạng thái" />
@@ -29,6 +34,7 @@ const ApproveMember = ({ value, candidateId, socket }: ApproveMemberProps) => {
                     <SelectItem value="WAITING">Chờ xử lý</SelectItem>
                     <SelectItem value="PASSED">Duyệt</SelectItem>
                     <SelectItem value="FAILED">Loại</SelectItem>
+                    <SelectItem value="REDO">Làm lại</SelectItem>
                 </SelectGroup>
             </SelectContent>
         </Select>
